742f02f37d59f1b047cb1d595a811447d3361b7d
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-03-23 15:16:46 +0530

    scripts: Add some handfull scripts
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I4b1bb263fc7e6d2b9df9b2095ce3168208bf0ddc

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# script
+Run Commands
+------------
+
+* For Git Configuration -
+
+```bash
+      wget https://raw.githubusercontent.com/Jayant-Deshmukh/script/master/git.sh && bash git.sh
+```
+
+* For Superior -
+
+```bash
+      wget https://raw.githubusercontent.com/Jayant-Deshmukh/script/master/superior.sh && bash superior.sh
+```
+
+* It will clone all files automatically.
---
+++ superior.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Copyright (C) 2015 Paranoid Android Project
+# Copyright (C) 2018-2020 Sipun Ku Mahanta<sipunkumar85@gmail.com>
+# Copyright (C) 2018-2020 Sweeto143@github / Darkstar085@XDA
+
+# PA Colors
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_YLW=$CLR_RST$(tput setaf 3)             #  yellow, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_PPL=$CLR_RST$(tput setaf 5)             #  purple,plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_YLW=$CLR_RST$CLR_BLD$(tput setaf 3) #  yellow, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_PPL=$CLR_RST$CLR_BLD$(tput setaf 5) #  purple, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+
+echo -e ""
+echo -e "${CLR_BLD_RED}██████╗  █████╗ ██████╗ ██╗  ██╗███████╗████████╗ █████╗ ██████╗ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_RED}██║  ██║███████║██████╔╝█████╔╝ ███████╗   ██║   ███████║██████╔╝${CLR_RST}"
+echo -e "${CLR_BLD_RED}██║  ██║██╔══██║██╔══██╗██╔═██╗ ╚════██║   ██║   ██╔══██║██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_RED}██████╔╝██║  ██║██║  ██║██║  ██╗███████║   ██║   ██║  ██║██║  ██║${CLR_RST}"
+echo -e "${CLR_BLD_RED}╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝${CLR_RST}"
+echo -e ""
+
+echo -e "${CLR_BLD_RED}Cloning device repos...${CLR_RST}"
+rm -rf device/asus/X01BD
+rm -rf kernel/asus/sdm660
+rm -rf vendor/asus/X01BD
+git clone https://github.com/SuperiorOS-Devices/device_asus_X01BD.git -b ten device/asus/X01BD
+git clone https://github.com/SuperiorOS-Devices/kernel_asus_X01BD.git -b ten kernel/asus/sdm660
+git clone https://github.com/SuperiorOS-Devices/vendor_asus_X01BD.git -b ten vendor/asus/X01BD
+echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Checking if Bringuped Properly...${CLR_RST}"
+echo -e ""
+. build/envsetup.sh
+lunch superior_X01BD-userdebug
+echo -e ""
+echo -e "${CLR_BLD_RED}Cloning Building Script...${CLR_RST}"
+echo -e ""
+rm -rf script_build.sh
+wget https://raw.githubusercontent.com/PixysOS/PixysOS_jenkins/ten/scripts/script_build.sh
+echo -e "${CLR_BLD_RED}Now You are good to Go${CLR_RST}"
+echo -e ""
+echo -e ""
